232f10025473236a715b5fc86a7fc1224bc1319c
mikaelaalves <mikasa201642@gmail.com>
2017-05-24 19:46:04 -0300

    atualização

--- DiadoAno/DiadoAno/Program.cs
+++ DiadoAno/DiadoAno/Program.cs
@@ -20,27 +20,27 @@ namespace DiadoAno
             if (mes  == "jane")
                 soma=dia;
             else if (mes == "feve")
-                soma = dia + 30;
+                soma = dia + 31;
             else if (mes == "març")
-                soma = dia + 60;
+                soma = dia + 31 + 28;
             else if (mes == "abri")
-                soma = dia + 90;
+                soma = dia + 31 + 28 + 31;
             else if (mes == "maio")
-                soma = dia + 120;
+                soma = dia + 31 + 28 + 31 + 30;
             else if (mes == "junh")
-                soma = dia + 150;
+                soma = dia + 31 + 28 + 31 + 30 + 31;
             else if (mes == "julh")
-                soma = dia + 180;
+                soma = dia + 31 + 28 + 31 + 30 + 31+ 30;
             else if (mes == "agos")
-                soma = dia + 210;
+                soma = dia + 31 + 28 + 31 + 30 + 31 + 30+ 31;
             else if (mes == "sete")
-                soma = dia + 240;
+                soma = dia + 31 + 28 + 31 + 30 + 31 + 30 + 31+ 31;
             else if (mes == "outu")
-                soma = dia + 270;
+                soma = dia + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31+ 30;
             else if (mes == "nove")
-                soma = dia + 300;
+                soma = dia + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30+ 31;
             else if (mes == "deze")
-                soma = dia + 330;
+                soma = dia + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31+ 30;
 
                     Console.WriteLine("O dia do ano é {0}º", soma);
               
