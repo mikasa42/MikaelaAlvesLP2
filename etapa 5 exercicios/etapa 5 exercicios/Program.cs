b05f30a5ee21d9c095db2c1832eb7979b934cd36
mikaelaalves <mikasa201642@gmail.com>
2017-05-24 20:23:15 -0300

    atualização

--- etapa 5 exercicios/etapa 5 exercicios/Program.cs
+++ etapa 5 exercicios/etapa 5 exercicios/Program.cs
@@ -10,41 +10,52 @@ namespace etapa_5_exercicios
     {
         static void Main(string[] args)
         {
+
+        } 
             struct carro
         {
             public string modelo;
             public int pot;
             public int km;
+            public int anofab;
+            public int fabricacao;
+            public string cor ;
         }
         static void Main(string[] args)
         {
                     string modelo, tipo;
                     int km,pot, carros;
-                    int carro;
                     
                     
-                    carro car1;
+                    
+                    
                     Console.WriteLine("Insira o numero de carros");
                     carros = Convert.ToInt32(Console.ReadLine());
-                    for (int i = 0; i < carros; i++)
-                    {
-                        int posicao[] = new int [i];
+                    {  
+                    car1[] carro = new carro [i];
+                            for (int i = 0; i < carros; i++)
+                            {
+                        
 
-                        Console.WriteLine("Insira o seu modelo ");
-                        car1.modelo = Console.ReadLine();
-                        Console.WriteLine("Insira a quilometragem :");
-                        car1.km = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Insira a potência :");
-                        car1.pot = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine("Insira o seu modelo ");
+                                car1.modelo = Console.ReadLine();
+                                Console.WriteLine("Insira a quilometragem :");
+                                car1.km = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine("Insira a potência :");
+                                car1.pot = Convert.ToInt32(Console.ReadLine());
 
-                        tipo = classif(car1.modelo, car1.km, car1.pot);
+                                tipo = classif(car1.modelo, car1.km, car1.pot);
 
+                             
+                            }
+                        for (int i = 0; i < carros; i++)
                         Console.WriteLine(tipo);
+                        Console.WriteLine("A taxa de uso é de {0}", taxa);
                     }
 
 
                 }
-                static string classif(string modelo, int km1, int pot1)
+                public static string classif(carro c)
                 {
                     string classrodagem, classpot, tipo;
 
@@ -74,5 +85,23 @@ namespace etapa_5_exercicios
                     tipo = String.Format("{0} - {1} - {2}", modelo, classrodagem, classpot);
                     return tipo;
         }
+                public static void cor(carro c)
+                {
+                    c.km = Console.ReadLine();
+                }
+
+                public static void mod(carro c)
+                {
+                    c.modelo = Console.ReadLine();
+                }
+
+                public static int taxadeuso(carro c)
+                {
+                    int anofab, anohj, taxa=0;
+                    taxa = anofab - anohj;
+
+                    return taxa;
+
+                }
     }
 }
