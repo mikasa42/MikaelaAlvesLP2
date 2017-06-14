1c84d1f15739a28dafada64dd4cfee0284ed14dd
mikaelaalves <mikasa201642@gmail.com>
2017-06-13 22:50:17 -0300

    atualização

--- Meu-intparse/Meu-intparse/Program.cs
+++ Meu-intparse/Meu-intparse/Program.cs
@@ -8,43 +8,23 @@ namespace Meu_intparse
 {
     class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
-            string n;
-            int  a=0, convert;
-            Console.WriteLine("informe seus valores :");
-            n = Console.ReadLine();
-
-            
-            }
-            convert =IntParse(n);
-            Console.WriteLine("O resultado é ", result);
+            Console.WriteLine(2 * MeuIntParse(Console.ReadLine()));
         }
-        public static int IntParse(string n)
+
+        public static int MeuIntParse(string num)
         {
-           int a=0, result=0;
-            for (int i=0; i<=n.Length; i++)
-                    {
-                        if (n[i] == '0')
-                            a = 0;
-                        if (n[i] == '1')
-                            a = 1;
-                        if (n[i] == '2')
-                            a = 2;
-                        if (n[i] == '3')
-                            a = 3;
-                        if (n[i] == '4')
-                            a = 4;
-                        if (n[i] == '5')
-                            a = 5;
-                        if (n[i] == '6')
-                            a = 6;
-                        if (n[i] == '7')
-                            a = 7;
-                        if (n[i] == '8')
-                            a = 8;
-                        if (n[i] == '9')
-                            a = 9;
+            int mult = 1;
+            int total = 0;
+
+            for (int i=num.Length-1; i >= 0; i--) {
+                total += (num[i] - '0') * mult;
+                mult *= 10;
+            }
+            
+            return total;
+        }
                     
                 
         }
---
+++ competição_de_corrida/competição_de_corrida/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace competição_de_corrida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
