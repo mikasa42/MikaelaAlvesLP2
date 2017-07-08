85170fd066f394ef4d64b99a2d026c90b810bf15
mikaelaalves <mikasa201642@gmail.com>
2017-07-08 17:28:53 -0300

    ATUALIZAÇÃO

---
+++ 2C/2-LivrosHermione/2-LivrosHermione/Livros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_LivrosHermione
+{
+    class Livros
+    {
+        public string Livro;
+        public int pags;
+        public int paghora;
+        public int horadia;
+        public int diasrest;
+        public int demora=0;
+        
+        public void calculando()
+        {
+            demora = (paghora * horadia) / pags;
+
+        }
+    }
+}
---
+++ 2C/2-LivrosHermione/2-LivrosHermione/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_LivrosHermione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livros livros;
+            livros = new Livros();
+
+            livros.Livro = Console.ReadLine();
+            livros.pags =int.Parse(Console.ReadLine());
+            livros.paghora = int.Parse(Console.ReadLine());
+            livros.horadia = int.Parse(Console.ReadLine());
+            livros.diasrest = int.Parse(Console.ReadLine());
+
+            livros.calculando();
+
+            if (livros.demora < livros.diasrest)
+            {
+                Console.WriteLine("Será posivel ler o livro em {0} dias ", livros.demora);
+            }
+            else
+            {
+                Console.WriteLine("Não será possível ler o livro ...");
+            }
+        }
+    }
+}
