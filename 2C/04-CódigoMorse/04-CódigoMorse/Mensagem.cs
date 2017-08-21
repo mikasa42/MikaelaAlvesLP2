3d8790c33a90fcc6f1e5eee74fd1677e309d9a5a
mikaelaalves <mikasa201642@gmail.com>
2017-08-21 16:07:29 -0300

    atualizaçao

---
+++ 2C/04-CódigoMorse/04-CódigoMorse/Mensagem.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_CódigoMorse
+{
+    class Mensagem
+    {
+        public string[] codguarda = new string[] { };
+         public string msg {get;set;}
+         public string[] tab = new string[]
+         {
+             ".-", "-...", "-.-.", "-..", ".", //a, b, c, d, e
+            "..-.", "--.", "....", "..", ".---", //f, g, h, i, j
+            "-.-", ".-..", "--", "-.", "---", //k, l, m, n, o
+            ".--.", "--.-", ".-.", "...", "-", //p, q, r, s, t
+            "..-", "...-", ".--", "-..-", "-.--", "--.." //u, v, w, x, y, z
+         };
+         public string[] alfabeto = new string[]
+        {
+            "a","b","c","d","e",
+            "f","g","h","i","j","k",
+            "l","m", "n","o","p","q","r",
+            "s","t","u","v","w","x","y","z"
+        };
+        
+        public string codificar()
+        {
+            string codigo= "";
+
+           for (int i = 0; i < msg.Length; i++)
+                {
+                    char letra = msg[i];
+                    int pos = letra - 'A';
+                    string codLetra = tab[pos];
+                    codigo += codLetra + "  ";
+
+                }
+               
+    
+          return codigo ;
+
+        }
+        public string decodificar()
+        {
+            
+                 
+                 string codigo = "", codLetra=" ";
+                 
+            
+            
+             string [] Letras= msg.Split(' '); 
+
+               for (int i = 0; i <msg.Length; i++)
+                {
+                    for( int k = 0; k <26; k++)
+                    {
+                        if(Letras[i] == tab[k] )
+                           {
+                              
+                              
+                              codLetra = alfabeto[i];
+                              
+                    
+                            }
+                           
+                    }
+                    codigo += codLetra + "  ";
+                }
+         return codigo;
+
+         }
+
+
+    }
+}
---
+++ 2C/04-CódigoMorse/04-CódigoMorse/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_CódigoMorse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("0-sair");
+            Console.WriteLine("1-novo codigo?");
+            
+
+            int op=int.Parse(Console.ReadLine());
+
+            Mensagem x = new Mensagem();
+
+            while (op == 1)
+            {
+                Console.WriteLine("2-codificar");
+                Console.WriteLine("3-decodificar");
+                Console.WriteLine("4-transmitir");
+
+                int y = int.Parse(Console.ReadLine());
+                if (y == 2)
+                {
+                    x.msg = Console.ReadLine();
+                    x.codificar();
+                    Console.WriteLine(x.codificar());
+                }
+                else if (y==3)
+                {
+                    x.msg = Console.ReadLine();
+                    x.decodificar();
+                    Console.WriteLine(x.decodificar());
+
+                }
+
+
+
+               
+            }
+        }
+    }
+}
