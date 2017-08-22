fd90eeca3289f5d7ad6b38262dad448e1536d876
mikasa42 <mikasa201642@gmail.com>
2017-08-22 15:57:55 -0300

    imcompleto

---
+++ 2C/6-AreaDoTriangulo/6-AreaDoTriangulo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AreaDoTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math.
+            
+
+        }
+    }
+}
---
+++ 2C/6-AreaDoTriangulo/6-AreaDoTriangulo/Triangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _6_AreaDoTriangulo
+{
+    class Triangulo
+    {
+        double[] cossenos = new double[] { 0.86, 0.7, 0.5 };
+
+        public double CalcularPerimetro(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+        
+        public double CalcularArea(double b, double h)
+        {
+            return (b * h) / 2;
+        }
+
+        public double CalcularArea(int b, double h)
+        {
+            return 1.0;
+        }
+    }
+}
